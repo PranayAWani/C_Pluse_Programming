256a6d18ef63a2a9231b9ded3d54fcab18fd1d5b
PranayAWani <pranaywani04@gmail.com>
2024-06-22 15:27:22 +0530

    update

--- bill.cpp
+++ bill.cpp
@@ -2,22 +2,41 @@
 using namespace std;
 
 class bill{
+    private:
+    void database{
+        
+    }
     public:
-    void top{
+    void top(){
         cout<<"\n      Jamuna prasad Fruit Supply pvt. ltd";
         cout<<"\n             holesale only ";
-        cout<<"\n ************************************************";
+        cout<<"\n***********************************************";
     }
-    void bottom{
-        int fno;
-        char fruites[fno][10];
-        cout<<"\n Enter the number of types of fruites you want to buy:- ";
-        cin>>fno;
-        for(int i=0;i<fno;i++){
-            cin<<fruites[i];
-        }
+    
+    void bottom();
+    void calculation{
+
     }
 };
+void bill::bottom() {
+    int fno;
+    cout << "\nEnter the number of types of fruits you want to buy:- ";
+    cin >> fno;
+
+    if (fno <= 0) {
+        cout << "The number of types of fruits should be a positive integer." << endl;
+        return;
+    }
+
+    string* fruits = new string[fno];
+    cout << "Enter the names and quatntity of the fruits:" << endl;
+    int i,j=fno;
+    int quantity[j];
+    for (i = 0; i < fno; ++i) {
+        cout << "Fruit " << (i + 1) << ": ";
+        cin >> fruits[i] >> quantity[i];
+    }
+    }
 int main(){
     bill b;
     b.bottom();
