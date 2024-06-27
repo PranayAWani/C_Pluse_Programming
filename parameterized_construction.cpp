5ac074267552fb3771d2dbfe245f6811da036013
PranayAWani <pranaywani04@gmail.com>
2024-06-27 20:33:30 +0530

    update

--- parameterized_construction.cpp
+++ parameterized_construction.cpp
@@ -3,7 +3,8 @@ using namespace std;
 class employ{
     int id;
     string name;
-    float sal
+    float sal;
+    public:
     employ(){
         id= 101;
         name = "Pranay"; 
@@ -19,11 +20,11 @@ class employ{
         name = "Aditi";
         sal =s;
     }
-    employ(float s, int i){
-        id = i;
-        name = "Shlok";
-        sal =s;
-    }
+    // employ(float s, int i){
+    //     id = i;
+    //     name = "Shlok";
+    //     sal =s;
+    // }
     employ(int i, string n ,float s){
         id=i;
         name = n;
@@ -33,8 +34,14 @@ class employ{
         cout<<"\n Employ id:- "<<id;
         cout<<"\n Employ name:- "<<name;
         cout<<"\n Employ salary:- "<<sal;
+        cout<<"\n *************************";
     }
 };
 int main(){
-    employ e1,e2(102)
+    employ e1,e2(102),e3(103,25000),e4(105,"Akshata",27000);
+    e1.print();
+    e2.print();
+    e3.print();
+    e4.print();
+    // e5.print();
 }
