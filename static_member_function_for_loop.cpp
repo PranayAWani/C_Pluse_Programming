a72a6ad6bb8aa616d64c6481ebd66a4e7fec02da
PranayAWani <pranaywani04@gmail.com>
2024-07-06 13:20:26 +0530

    update

--- static_member_function_for_loop.cpp
+++ static_member_function_for_loop.cpp
@@ -15,5 +15,30 @@ class shop{
         amt=rate*qty;
         totalamt=amt+totalamt;
     }
-    void put()
+    void put(){
+        cout<<"\n"<<id<<"\t"<<name<<"\t"<<qty<<"\t"<<amt;
+    }
+    static void total_amt(){
+        cout<<"\n Total Bill:- "<<totalamt;
+    }
 };
+float shop::totalamt;
+int main(){
+    int no,i;
+    shop *s1, *s2;
+    cout<<"\n Enter the number of products:- ";
+    cin>>no;
+    s1=new shop[no];
+    s2=s1;
+    for(i=0;i<no;i++){
+        s1->get();
+        s1->cal();
+        s1++;
+    }
+    for(i=0;i<no;i++){
+        s2->put();
+        s2++;
+    }
+    cout<<"\n _________________";
+    shop::total_amt();
+}
