e403fdcb8828172e964cd717b697221e9696159d
PranayAWani <pranaywani04@gmail.com>
2024-07-07 14:26:16 +0530

    update

---
+++ multiple_oilcans_boo.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+class oilcan{
+    int ltr ,ml;
+    void get(){
+        cout<<"\n Enter thr volume:- ";
+        cin>>ltr>>ml;
+    }
+    void put(){
+        cout<<"\n"<<ltr<<"\t"<<ml;
+    }
+    oilcan operator +(oilcan m){
+        oilcan 
+    }
+};
