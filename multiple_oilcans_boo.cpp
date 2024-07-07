54ac89109a2972ece6ceea2b02e2a2da4975056b
PranayAWani <pranaywani04@gmail.com>
2024-07-07 15:51:21 +0530

    multiple oilcan

--- multiple_oilcans_boo.cpp
+++ multiple_oilcans_boo.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 class oilcan{
     int ltr ,ml;
+    public:
+    oilcan(){
+        ml=0;
+        ltr=0;
+    }
     void get(){
         cout<<"\n Enter thr volume:- ";
         cin>>ltr>>ml;
@@ -10,6 +15,24 @@ class oilcan{
         cout<<"\n"<<ltr<<"\t"<<ml;
     }
     oilcan operator +(oilcan m){
-        oilcan 
+        oilcan temp;
+        temp.ml=ml+m.ml;
+        temp.ltr=ltr+m.ltr+temp.ml/1000;
+        temp.ml=temp.ml%1000;
+        return(temp);
     }
 };
+int main(){
+    int no,i;
+    cout<<"\n Enter the number of containers:- ";
+    cin>>no;
+    oilcan o[no];
+    oilcan t1;
+    for (i=1;i<=no;i++){
+        cout<<"\n Enter the amout of liquid in "<<i<<" container:- ";
+        o[i].get();
+        o[i+1]=o[i]+t1;
+        t1 = o[i+1];
+    }
+    t1.put();
+}
